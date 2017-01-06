c79be13a6e2cb67f9805ef30f903cbe28cdb63dc
Javi Fontan <jfontan@opennebula.org>
2017-01-07 00:23:31 +0100

    Fix first boot script
    * Wait until ssh is ready
    * Wait for OpenNebula to start
    * Add connection info to motd

--- firstboot-opennebula.sh
+++ firstboot-opennebula.sh
@@ -2,6 +2,12 @@
 
 SCRIPT_PATH="$0"
 
+# Wait until ssh is ready
+while ! (ssh-keyscan localhost | grep localhost -q); do
+    echo "Waiting for ssh" 1>&2
+    sleep 1
+done
+
 # Get ssh fingerprint from localhost so it is not asked
 su oneadmin -c "ssh-keyscan localhost > ~/.ssh/known_hosts"
 
@@ -14,11 +20,15 @@ echo "oneadmin:$PASSWORD" > /var/lib/one/.one/one_auth
 # Enable and start services
 systemctl enable libvirtd --now
 systemctl enable opennebula --now
+
+# Wait for OpenNebula daemon to be ready
+while ! oneuser show > /dev/null; do
+    sleep 1
+done
+
 systemctl enable opennebula-sunstone --now
 systemctl enable opennebula-flow --now
 systemctl enable opennebula-gate --now
-
-sleep 5
 
 # Change datastore drivers
 EDITOR="sed -i 's/ssh/qcow2/'" onedatastore update 0
@@ -37,6 +47,20 @@ EOT
 
 onevnet create /tmp/network
 onevnet addar net --ip 192.168.122.50 --size 200
+
+onehost create localhost --im kvm --vm kvm
+
+IP=$(scw-metadata PUBLIC_IP_ADDRESS)
+
+(
+echo ""
+echo "OpenNebula is ready. You can connect to http://$IP:9869"
+echo "use the following credentials:"
+echo ""
+echo "Username: oneadmin"
+echo "Password: $PASSWORD"
+echo ""
+) > /etc/motd
 
 rm -f $SCRIPT_PATH
 
